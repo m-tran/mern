2f418b4192f9cd2c4cb9c1553091846895602012
Michelle Tran <mptran0101@berkeley.edu>
2020-08-10 19:57:37 -0700

    Updates

---
+++ precache-manifest.d27a1880579f33774ad3fe3eac0020bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e766acb88fcef583250bd58bdcfd6dd4",
+    "url": "/mern/index.html"
+  },
+  {
+    "revision": "3c6558f4d2856e79302a",
+    "url": "/mern/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/mern/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mern/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6558f4d2856e79302a",
+    "url": "/mern/static/js/main.fd4f4f07.chunk.js"
+  },
+  {
+    "revision": "21930d7dec812ba9c7d4",
+    "url": "/mern/static/js/runtime-main.c6c2aad9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mern/static/media/logo.5d5d9eef.svg"
+  }
+]);
